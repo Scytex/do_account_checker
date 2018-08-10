d947ae79965dccba5e7eac76eac80b2927e47540
Requi <privat@requi-dev.de>
2018-08-10 21:04:11 +0200

    Fixed stupid usernames and passwords.

--- do_account_checker/DarkOrbit/User.cs
+++ do_account_checker/DarkOrbit/User.cs
@@ -82,7 +82,7 @@ namespace do_account_checker.DarkOrbit
             var mainPage = _webRequestHandler.Get("https://www.darkorbit.com").Content;
             var authUrl = WebUtility.HtmlDecode(mainPage.GetBetween("class=\"bgcdw_login_form\" action=\"", "\""));
 
-            var response = _webRequestHandler.Post(authUrl, new KeyValuePair<string, object>("username", Username), new KeyValuePair<string, object>("password", Password));
+            var response = _webRequestHandler.Post(authUrl, new KeyValuePair<string, object>("username", WebUtility.HtmlEncode(Username)), new KeyValuePair<string, object>("password", WebUtility.HtmlEncode(Password)));
             _content = response.Content;
             _uri = response.ResponseUri.AbsoluteUri;
             var status = _uri.Contains("indexInternal.es");
